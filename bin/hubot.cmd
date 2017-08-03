04b2f4383d878514f0a98df2b2dbfdf11d813c53
davi2137 <davi2137@gmail.com>
2017-08-03 17:37:32 +0800

    SET

--- bin/hubot.cmd
+++ bin/hubot.cmd
@@ -3,5 +3,6 @@
 call npm install
 SETLOCAL
 SET PATH=node_modules\.bin;node_modules\hubot\node_modules\.bin;%PATH%
+SET HUBOT_SLACK_TOKEN="xoxb-222440086742-SQUQ4ALKRfJ5C0mcRs3dMCMm"
 
 node_modules\.bin\hubot.cmd --name "KnowBon" %* 
