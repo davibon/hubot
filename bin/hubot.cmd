620c0a2ec99c3d6f88d41a1129133e8b5882019e
davi2137 <davi2137@gmail.com>
2017-08-04 08:33:49 +0800

    revise

--- bin/hubot.cmd
+++ bin/hubot.cmd
@@ -3,7 +3,4 @@
 call npm install
 SETLOCAL
 SET PATH=node_modules\.bin;node_modules\hubot\node_modules\.bin;%PATH%
-SET LINE_CHANNEL_ID="1528330189"
-SET LINE_CHANNEL_SECRET="d022c13516f97dad0e4700df125887b0"
-SET LINE_CHANNEL_MID="Ucba5128fb34e590e0914414ab0082124"
 node_modules\.bin\hubot.cmd --name "KnowBon" %* 
